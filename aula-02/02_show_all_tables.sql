d4cb362ecfd00d920bc9e9e0c7440f4ff7a5eb6c
Thiagooms <thiagopaulista50@gmail.com>
2026-01-19 15:15:49 -0300

    aula 02: aprendendo conceitos de FROM na pratica

---
+++ aula-02/02_show_all_tables.sql
@@ -0,0 +1,9 @@
+.tables
+
+/* 
+NO POSTGRESQL:
+    \dt no PostgreSQL: mostra todas as tabelas do banco
+    \dt *.*: lista tabelas de todos os schemas
+    \dt+ nome_tabela: mostra detalhes de uma tabela específica
+    \d nome_tabela: descreve a estrutura de uma tabela
+*/
---
+++ aula-02/03_select_from.sql
@@ -0,0 +1,4 @@
+SELECT *
+FROM produtos
+LIMIT 1
+--LIMIT sempre vai ser a ultima coisa da querie
